e2c5140647f2e498097703e4bec4100700c4556c
Chris Klinedinst <chris.klinedinst@solarwinds.cloud>
2019-04-16 14:29:37 -0500

    Update support.rb

--- lib/lita/handlers/support.rb
+++ lib/lita/handlers/support.rb
@@ -21,6 +21,9 @@ module Lita
             },
             organization_id: {
               short: "u"
+            },
+            name: {
+              short: "n"
             }
           }
         )
